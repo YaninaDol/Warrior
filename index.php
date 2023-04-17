623bb17c0ba7e99d85739fc95d4f567f5d433bd2
YaninaDol <yanina.dolzhenko@gmail.com>
2023-04-17 19:47:57 +0400

    add horse

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include 'mounted.php';
 include 'footSoldier.php';
 
 
-$soldier =new Mounted(new Horse(100));
+$soldier =new Mounted(new Horse(50));
 $soldier->attack($soldier->getBow());
 $soldier->defend($soldier->getSword());
 $soldier->move();
